620d322a28a2d10ba630c74b6529936323765c0e
Christoph <christoph@christoph-laptop.local>
2019-04-13 23:25:52 -0700

    wip

--- index.js
+++ index.js
@@ -10,82 +10,192 @@ const FS = require("fs");
 const PATH = require("path");
 let VERBOSE = !!process.env.VERBOSE;
 
-function makeLIBFor (doc, baseDir) {
-
-    if (typeof doc === 'string') {
-        doc = JSON.parse(doc);
-    }
-    if (!doc.js) {
-        throw new Error(`No 'js' exports found in doc!`);
-    }
-
-    const packages = doc.js;
+function makeLIBFor (doc, baseDir, parentLib) {
 
     if (!baseDir) {
         throw new Error(`No 'baseDir' set!`);
     }
 
-    Object.keys(packages).forEach(function (name) {
-        const upperName = name.toUpperCase().replace(/[\.-]/g, "_");
-        if (!/^[A-Z0-9_]+$/.test(upperName)) {
-            return;
-        }
-        const path = PATH.resolve(baseDir, packages[name]);
-        packages[upperName] = packages[name] = path;
-    });
-
-    function resolve (id) {
-        id = id.split('/');
-        if (!packages[id[0]]) {
-            return require('resolve').sync(id.join("/"), {
-                basedir: baseDir
-            });
-        }
-        return PATH.join(packages[id[0]], id.slice(1).join("/"));
+    if (typeof doc === 'string') {
+        doc = JSON.parse(doc);
     }
 
-    const LIB = {
-        require: function (id) {
-            return require(resolve(id));
-        },
-        resolve: function (id) {
-            return resolve(id);
-        }
-    };
-    const libNative = {};
-    Object.keys(LIB).forEach(function (name) {
-        libNative[name] = true;
-    });
+    const LIB = {};
 
-    return new Proxy(LIB, {
-        get (target, name, receiver) {
-            if (
-                typeof name === 'symbol' ||
-                name === 'inspect' ||
-                libNative[name]
-            ) {
-                return Reflect.get(target, name, receiver);
+    if (doc.bin) {
+        const binPackages = doc.bin;
+
+        function resolve (id) {
+            id = id.split('/');
+            if (!binPackages[id[0]]) {
+                throw new Error(`Cannot resolve bin '${id.join('/')}'`);
             }
-            return LIB.require(name);
+            return PATH.join(baseDir, binPackages[id[0]], id.slice(1).join("/"));
+        }
+
+        let lib = {
+            /*
+            require: function (id) {
+                // TODO: Create wrapper that can exec command
+            },
+            */
+            resolve: function (id) {
+                return resolve(id);
+            }
+        };
+        const libNative = {};
+        Object.keys(lib).forEach(function (name) {
+            libNative[name] = true;
+        });
+
+        LIB.bin = new Proxy(lib, {
+            get (target, name, receiver) {
+                if (
+                    typeof name === 'symbol' ||
+                    name === 'inspect' ||
+                    libNative[name]
+                ) {
+                    return Reflect.get(target, name, receiver);
+                }
+                return lib.resolve(name);
+            }
+        });
+    }
+
+    if (doc.js) {
+        const jsPackages = doc.js;
+
+        Object.keys(jsPackages).forEach(function (name) {
+            const upperName = name.toUpperCase().replace(/[\.-]/g, "_");
+            if (!/^[A-Z0-9_]+$/.test(upperName)) {
+                return;
+            }
+            const path = PATH.resolve(baseDir, jsPackages[name]);
+            jsPackages[upperName] = jsPackages[name] = path;
+        });
+
+        function resolve (_id) {
+            const id = _id.split('/');
+            if (!jsPackages[id[0]]) {
+                try {
+                    if (VERBOSE) console.error('[lib.json] resolve(_id)', _id, 'using', 'baseDir', baseDir);
+                    return require('resolve').sync(id.join("/"), {
+                        basedir: baseDir
+                    });
+                } catch (err) {
+                    if (parentLib) {
+                        if (VERBOSE) console.error('[lib.json] resolve(_id)', _id, 'using', 'parent');
+                        return parentLib.js.resolve(_id);
+                    }
+                    try {
+                        // Check if it is a native module.
+                        return require.resolve(_id.toLowerCase());
+                    } catch (err) {}
+                    throw err;
+                }
+            }
+            const path = PATH.resolve(baseDir, jsPackages[id[0]], id.slice(1).join("/"));
+            if (VERBOSE) console.error('[lib.json] resolve(_id)', _id, 'return', path);
+            return path;
+        }
+
+        let lib = {
+            require: function (id) {
+                return require(resolve(id));
+            },
+            resolve: function (id) {
+
+                if (VERBOSE) console.error('[lib.json] lib.js.resolve(id)', id);
+
+                return resolve(id);
+            }
+        };
+        const libNative = {};
+        Object.keys(lib).forEach(function (name) {
+            libNative[name] = true;
+        });
+
+        LIB.js = new Proxy(lib, {
+            get (target, name, receiver) {
+                if (
+                    typeof name === 'symbol' ||
+                    name === 'inspect' ||
+                    libNative[name]
+                ) {
+                    return Reflect.get(target, name, receiver);
+                }
+                return lib.require(name);
+            }
+        });
+
+        Object.defineProperty(LIB, 'NODE_PATH', {
+            get: function () {
+                let paths = {};
+                if (parentLib) {
+                    parentLib.NODE_PATH.forEach(function (path) {
+                        paths[path] = true;
+                    });
+                } else
+                if (process.env.NODE_PATH) {
+                    process.env.NODE_PATH.split(':').forEach(function (path) {
+                        paths[path] = true;
+                    });
+                }
+                Object.keys(jsPackages).forEach(function (name) {
+                    paths[PATH.dirname(jsPackages[name])] = true;
+                });
+                return Object.keys(paths).reverse();
+            }
+        });
+    }
+
+    return LIB;
+}
+
+exports.forBaseDir = function (baseDir) {
+    const baseDirs = [];
+    const parts = baseDir.split('/')
+    for (let i=parts.length; i > 1; i--) {
+        baseDirs.push(parts.slice(0, i).join('/'));
+    }
+    return exports.forBaseDirs(baseDirs);
+}
+
+exports.forBaseDirs = function (baseDirs) {
+    const basePaths = [];
+    baseDirs.forEach(function (baseDir) {
+        const parts = baseDir.split('/')
+        for (let i=parts.length; i > 1; i--) {
+            basePaths.push(parts.slice(0, i).join('/'));
         }
     });
+    return exports.forBasePaths(basePaths);
+}
+
+exports.forBasePaths = function (basePaths) {
+    const paths = [];
+    basePaths.forEach(function (path) {
+        paths.push(PATH.join(path, '.~lib.json'));
+        paths.push(PATH.join(path, 'lib.json'));
+    });
+    let path = null;
+    let parentLib = null;
+    while ( (path = paths.pop()) ) {
+        if (
+            FS.existsSync(path) &&
+            FS.statSync(path).isFile()
+        ) {
+            parentLib = makeLIBFor(FS.readFileSync(path, 'utf8'), PATH.dirname(path), parentLib);
+        }
+    }
+    if (!parentLib) {
+        throw new Error(`Cannot locate '[.~]lib.json' for basePaths '${basePaths.join(', ')}'!`);
+    }
+    return parentLib;
 }
 
 exports.forModule = function (module) {
-
-    const paths = [];
-    module.paths.forEach(function (path) {
-        paths.push(PATH.join(path, '..', '.~lib.json'));
-        paths.push(PATH.join(path, '..', 'lib.json'));
-    });
-
-    let path = null;
-    while ( (path = paths.shift()) ) {
-        if (FS.existsSync(path)) {
-            return makeLIBFor(FS.readFileSync(path, 'utf8'), PATH.dirname(path));
-        }
-    }
-    throw new Error(`Cannot locate '[.~]lib.json' for module '${module.id}'!`);
+    return exports.forBaseDir(PATH.dirname(module.id));
 }
 
 exports.forDoc = function (doc, baseDir) {
@@ -134,7 +244,6 @@ if (require.main === module) {
             cwd = PATH.resolve(cwd, args.cwd);
             process.chdir(cwd);
         }
-
         if (
             args._.length === 2 &&
             args._[0] === 'from' &&
@@ -143,21 +252,31 @@ if (require.main === module) {
             const doc = await exports.docFromNodeModules(cwd);
 
             process.stdout.write(JSON.stringify(doc, null, 4));
+        } else
+        if (args._[0] === 'resolve.bin') {
+            const path = exports.forBaseDir(process.cwd()).bin.resolve(args._[1]);
+            if (!path) {
+                throw new Error(`ERROR: Could not resolve bin path for '${args._[1]}'`);
+            }
+            process.stdout.write(path);
+        } else
+        if (args._[0] === 'resolve.js') {
+            const path = exports.forBaseDir(process.cwd()).js.resolve(args._[1]);
+            if (!path) {
+                throw new Error(`ERROR: Could not resolve js path for '${args._[1]}'`);
+            }
+            process.stdout.write(path);
         } else {
             throw new Error(`[lib.json] ERROR: Command not supported!`);
         }
     }    
-    try {
-        main(MINIMIST(process.argv.slice(2), {
-            boolean: [
-                'verbose',
-                'debug'
-            ]
-        })).catch(function (err) {
-            throw err;
-        });
-    } catch (err) {
+    main(MINIMIST(process.argv.slice(2), {
+        boolean: [
+            'verbose',
+            'debug'
+        ]
+    })).catch(function (err) {
         console.error("[lib.json] ERROR:", err.stack);
         process.exit(1);
-    }
+    });
 }
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 
 const ASSERT = require('assert');
+const PATH = require('path');
 const FS = require('fs');
 const LIB = require('.');
 const EXEC = require('child_process').execSync;
@@ -7,38 +8,105 @@ const EXEC = require('child_process').execSync;
 
 describe('lib.json', function () {
 
-    it('forDoc', function () {
-        const lib = LIB.forDoc(`{
-            "js": {
-                "lib.json": "."
-            }
-        }`, __dirname);
-        ASSERT.deepEqual(Object.keys(lib), [
-            'require',
-            'resolve'
-        ]);
-        ASSERT.equal(lib.resolve('lib.json'), __dirname);
-        ASSERT.equal(lib.resolve('LIB_JSON'), __dirname);
-        ASSERT.equal(lib['lib.json'] === LIB, true);
-        ASSERT.equal(lib['LIB_JSON'] === LIB, true);
+    describe('js', function () {
+
+        it('forDoc', function () {
+            const lib = LIB.forDoc(`{
+                "js": {
+                    "lib.json": "."
+                }
+            }`, __dirname).js;
+            ASSERT.deepEqual(Object.keys(lib), [
+                'require',
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), __dirname);
+            ASSERT.equal(lib.resolve('LIB_JSON'), __dirname);
+            ASSERT.equal(lib['lib.json'] === LIB, true);
+            ASSERT.equal(lib['LIB_JSON'] === LIB, true);
+        });
+
+        it('forModule', function () {
+            FS.writeFileSync('.~lib.json', `{
+                "js": {
+                    "lib.json": "."
+                }
+            }`, 'utf8');
+            const lib = LIB.forModule(module).js;
+            FS.unlinkSync('.~lib.json');
+            ASSERT.deepEqual(Object.keys(lib), [
+                'require',
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), __dirname);
+            ASSERT.equal(lib.resolve('LIB_JSON'), __dirname);
+            ASSERT.equal(lib['lib.json'] === LIB, true);
+            ASSERT.equal(lib['LIB_JSON'] === LIB, true);
+        });
+
+        it('forBaseDir', function () {
+            FS.writeFileSync('.~lib.json', `{
+                "js": {
+                    "lib.json": "."
+                }
+            }`, 'utf8');
+            const lib = LIB.forBaseDir(__dirname).js;
+            FS.unlinkSync('.~lib.json');
+            ASSERT.deepEqual(Object.keys(lib), [
+                'require',
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), __dirname);
+            ASSERT.equal(lib.resolve('LIB_JSON'), __dirname);
+            ASSERT.equal(lib['lib.json'] === LIB, true);
+            ASSERT.equal(lib['LIB_JSON'] === LIB, true);
+        });
     });
 
-    it('forModule', function () {
-        FS.writeFileSync('.~lib.json', `{
-            "js": {
-                "lib.json": "."
-            }
-        }`, 'utf8');
-        const lib = LIB.forModule(module);
-        FS.unlinkSync('.~lib.json');
-        ASSERT.deepEqual(Object.keys(lib), [
-            'require',
-            'resolve'
-        ]);
-        ASSERT.equal(lib.resolve('lib.json'), __dirname);
-        ASSERT.equal(lib.resolve('LIB_JSON'), __dirname);
-        ASSERT.equal(lib['lib.json'] === LIB, true);
-        ASSERT.equal(lib['LIB_JSON'] === LIB, true);
+    describe('bin', function () {
+
+        it('forDoc', function () {
+            const lib = LIB.forDoc(`{
+                "bin": {
+                    "lib.json": "index.js"
+                }
+            }`, __dirname).bin;
+            ASSERT.deepEqual(Object.keys(lib), [
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), PATH.join(__dirname, 'index.js'));
+            ASSERT.equal(lib['lib.json'], PATH.join(__dirname, 'index.js'));
+        });
+
+        it('forModule', function () {
+            FS.writeFileSync('.~lib.json', `{
+                "bin": {
+                    "lib.json": "index.js"
+                }
+            }`, 'utf8');
+            const lib = LIB.forModule(module).bin;
+            FS.unlinkSync('.~lib.json');
+            ASSERT.deepEqual(Object.keys(lib), [
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), PATH.join(__dirname, 'index.js'));
+            ASSERT.equal(lib['lib.json'], PATH.join(__dirname, 'index.js'));
+        });
+
+        it('forBaseDir', function () {
+            FS.writeFileSync('.~lib.json', `{
+                "bin": {
+                    "lib.json": "index.js"
+                }
+            }`, 'utf8');
+            const lib = LIB.forBaseDir(__dirname).bin;
+            FS.unlinkSync('.~lib.json');
+            ASSERT.deepEqual(Object.keys(lib), [
+                'resolve'
+            ]);
+            ASSERT.equal(lib.resolve('lib.json'), PATH.join(__dirname, 'index.js'));
+            ASSERT.equal(lib['lib.json'], PATH.join(__dirname, 'index.js'));
+        });
     });
 
     it('fromNodeModules', function () {
@@ -50,6 +118,20 @@ describe('lib.json', function () {
         ASSERT.deepEqual(doc, {
             bin: 12,
             js: 192
+        });
+    });
+
+    describe('NODE_PATH', function () {
+
+        it('forDoc', function () {
+            const NODE_PATH = LIB.forDoc(`{
+                "js": {
+                    "lib.json": "."
+                }
+            }`, __dirname).NODE_PATH;
+            ASSERT.deepEqual(NODE_PATH, [
+                PATH.dirname(__dirname)
+            ].concat(process.env.NODE_PATH.split(':').reverse()));    
         });
     });
 
